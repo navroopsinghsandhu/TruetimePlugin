409e7e5bb111d383c9cc8330dd9275b4b5751281
navroopsinghsandhu <navroops806@gmail.com>
2021-11-25 20:06:28 +0530

    common javascript interface for communicating with the native code of two platforms - ios and android

---
+++ www/truetimeplugin.js
@@ -0,0 +1,21 @@
+// Empty constructor
+function TruetimePlugin() {}
+
+// The function that passes work along to native shells
+// Message is a string, duration may be 'long' or 'short'
+TruetimePlugin.prototype.getTrueTime = function(message, duration, successCallback, errorCallback) {
+  var options = {};
+  options.message = message;
+  options.duration = duration;
+  cordova.exec(successCallback, errorCallback, 'TruetimePlugin', 'getTime', [options]);
+}
+
+// Installation constructor that binds TruetimePlugin to window
+TruetimePlugin.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.truetimeplugin = new TruetimePlugin();
+  return window.plugins.truetimeplugin;
+};
+cordova.addConstructor(TruetimePlugin.install);
